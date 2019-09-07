b8726e56e81e42bd20ee28053aeef6121ee9ced5
sergiuilie <ilie.sergiu@ymail.com>
2019-09-07 04:37:50 +0200

    metadata 4 rferl

---
+++ src/main/java/com/rferl/SWEN/service/ScanRFERL4MetaService.java
@@ -0,0 +1,50 @@
+package com.rferl.SWEN.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author sergiuTheRomanian
+ * @implNote noRussianAllowed
+ */
+
+@Service
+public class ScanRFERL4MetaService {
+
+    public static boolean checkUrlForRFLRL(List<String> urlList) {
+        if (urlList.size() == 0) {
+            throw new IllegalArgumentException("URL List is empty, please provide at least one URL");
+        } else {
+            for (String url : urlList) {
+                try {
+                    Document doc = Jsoup.connect(url).get();
+                    Elements divTagTitle = doc.getElementsByAttributeValueContaining("class", "col-title");
+                    System.out.println(divTagTitle.text());
+                    Elements divTagPublishDate = doc.getElementsByAttributeValueContaining("class", "published");
+                    System.out.println(divTagPublishDate.text());
+                    Elements divTagPublisher = doc.getElementsByAttributeValueContaining("class", "links__item-link");
+                    System.out.println(divTagPublisher.text());
+                    Elements description = doc.select("span[class=caption]");
+                    System.out.println(description.text());
+                    for (Element desc : description
+                    ) {
+                        if (desc.text() != null) {
+                            String imgSrc = doc.select("img[alt=" + desc.ownText() + "]").attr("src");
+                            System.out.println(imgSrc + "\n\n");
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- src/test/java/com/rferl/SWEN/SwenApplicationTests.java
+++ src/test/java/com/rferl/SWEN/SwenApplicationTests.java
@@ -1,16 +1,35 @@
 package com.rferl.SWEN;
 
+import com.rferl.SWEN.service.ScanRFERL4MetaService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SwenApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void checkMetadata() {
+        List<String> urls = new ArrayList<String>();
+        urls.add("https://www.rferl.org/a/iran-confirms-to-eu-latest-step-away-from-nuclear-deal/30149693.html");
+        urls.add("https://www.rferl.org/a/u-s-lawmakers-condemn-russia-s-election-failure-crackdown-ahead-of-votes/30149641.html");
+        urls.add("https://www.rferl.org/a/how-a-local-vote-rocked-russia-moscow-election-caps-summer-of-discontent/30150471.html");
+        ScanRFERL4MetaService scanRFERL4MetaService = new ScanRFERL4MetaService();
+        scanRFERL4MetaService.checkUrlForRFLRL(urls);
+        Assert.assertTrue("Should return true: ", scanRFERL4MetaService.checkUrlForRFLRL(urls));
+
+    }
+
 
 }
